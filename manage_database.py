b4ed7cfe238a0af6f077d632078837c6f8607b1f
HTmake-code <fi1er01hkr10ty@gmail.com>
2024-04-05 13:18:28 +0900

    TEST:get_pages

---
+++ manage_database.py
@@ -0,0 +1,39 @@
+import requests
+from datetime import datetime, timezone
+
+import settings
+
+NOTION_API_TOKEN = settings.api_key
+DATABASE_ID = settings.db_id
+
+headers = {
+    "Authorization": "Bearer " + NOTION_API_TOKEN,
+    "Content-Type": "application/json",
+    "Notion-Version": "2022-06-28"
+}
+
+def get_pages():
+    url = f"https://api.notion.com/v1/databases/{DATABASE_ID}/query"
+
+    payload = {"page_size": 100}
+    response = requests.post(url, json=payload, headers=headers)
+
+    data = response.json()
+
+    import json
+    with open('db.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+    results = data["results"]
+    return results
+
+# test get_pages
+pages = get_pages()
+for page in pages:
+    page_id = page["id"]
+    props = page["properties"]
+    url = props["URL"]["title"][0]["text"]["content"]
+    title = props["Title"]["rich_text"][0]["text"]["content"]
+    published = props["Published"]["date"]["start"]
+    published = datetime.fromisoformat(published)
+    print(url, title, published)
