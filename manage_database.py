2d4409a565fa5bd06a6cb6e53f69bc71b66215e6
HTmake-code <fi1er01hkr10ty@gmail.com>
2024-04-08 15:59:13 +0900

    get_blocks and create_blocks. They don't work.

--- manage_database.py
+++ manage_database.py
@@ -27,10 +27,10 @@ def get_pages():
     results = data["results"]
     return results
 
-def create_page(data:dict, child_data:dict):
+def create_page(data:dict):
     create_url = "https://api.notion.com/v1/pages"
 
-    payload = {"parent": {"database_id": DATABASE_ID}, "properties": data, "children": child_data}
+    payload = {"parent": {"database_id": DATABASE_ID}, "properties": data}
 
     res = requests.post(create_url, headers=headers, json=payload)
     print(res.status_code)
@@ -54,6 +54,33 @@ def delete_page(page_id: str):
     print(res.status_code)
     return res
 
+def create_blocks(page_id: str, data: dict):
+    create_url = f"https://api.notion.com/v1/blocks/{page_id}/children"
+
+    payload = {"children": data}
+
+    res = requests.post(create_url, headers=headers, json=payload)
+    print(res.status_code)
+    return res    
+
+def get_blocks(page_id: str):
+    url = f"https://api.notion.com/v1/databases/{page_id}"
+
+    #payload = {"page_size": 100}
+    response = requests.post(url,headers=headers)
+
+    data = response.json()
+    print(response.status_code)
+
+    import json
+    with open('blocks.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
+
+    results = data["results"]
+    return results
+
+
+
 name = "name"
 title = "Test"
 race_date = datetime.now().astimezone(timezone.utc).isoformat()
@@ -61,8 +88,12 @@ data = {
     "Race": {"title": [{"text": {"content": name}}]},
     "RaceDate": {"date": {"start": race_date, "end": None}}
 }
+
 child_data = {
-    "heading_1": {"rich_text": [{"text": {"content": "この言葉は見えているか！"}}]}
+    #"heading_1": {"rich_text": [{"text": {"content": "Can you see that！"}}]}
 }
-create_page(data, child_data)
+page_id = "7e9ccfba-5a53-4d76-832e-6085783e93f0"
+#create_page(data)
 #get_pages()
+#create_blocks(page_id, child_data)
+get_blocks(page_id) 
