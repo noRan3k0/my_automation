7ba3aa46c7b4b2f6ae3ba4b2b19ac01a3cbffa5b
HTmake-code <fi1er01hkr10ty@gmail.com>
2024-04-08 16:17:04 +0900

    children関係の処理の削除と、page_idの読み込み

--- manage_database.py
+++ manage_database.py
@@ -1,10 +1,12 @@
 import requests
+import json
 from datetime import datetime, timezone
 
 import settings
 
 NOTION_API_TOKEN = settings.api_key
 DATABASE_ID = settings.db_id
+PAGE_ID = settings.page_id
 
 headers = {
     "Authorization": "Bearer " + NOTION_API_TOKEN,
@@ -12,6 +14,7 @@ headers = {
     "Notion-Version": "2022-06-28"
 }
 
+# データベースからページの情報取得
 def get_pages():
     url = f"https://api.notion.com/v1/databases/{DATABASE_ID}/query"
 
@@ -20,22 +23,23 @@ def get_pages():
 
     data = response.json()
 
-    import json
     with open('db.json', 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
 
     results = data["results"]
     return results
 
-def create_page(data:dict, child_data:dict):
+# データベースに新しいページを作成
+def create_page(data:dict):
     create_url = "https://api.notion.com/v1/pages"
 
-    payload = {"parent": {"database_id": DATABASE_ID}, "properties": data, "children": child_data}
+    payload = {"parent": {"database_id": DATABASE_ID}, "properties": data}
 
     res = requests.post(create_url, headers=headers, json=payload)
     print(res.status_code)
     return res
 
+# データベース上のページを更新
 def update_page(page_id: str, data: dict):
     url = f"https://api.notion.com/v1/pages/{page_id}"
 
@@ -45,6 +49,7 @@ def update_page(page_id: str, data: dict):
     print(res.status_code)
     return res
 
+# データベース上のページを削除
 def delete_page(page_id: str):
     url = f"https://api.notion.com/v1/pages/{page_id}"
 
@@ -53,6 +58,10 @@ def delete_page(page_id: str):
     res = requests.patch(url, headers=headers, json=payload)
     print(res.status_code)
     return res
+
+# ページから情報を取得
+
+
 
 name = "name"
 title = "Test"
@@ -64,5 +73,5 @@ data = {
 child_data = {
     "heading_1": {"rich_text": [{"text": {"content": "この言葉は見えているか！"}}]}
 }
-create_page(data, child_data)
+create_page(data)
 #get_pages()
