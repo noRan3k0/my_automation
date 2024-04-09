6c8d8b955f0cfa98c485e69213dd1a7e9569637e
HTmake-code <fi1er01hkr10ty@gmail.com>
2024-04-10 00:15:45 +0900

    make_dataに変更

--- make_data.py
+++ make_data.py
@@ -1,34 +1,246 @@
-data = []
-tableOfBlock = {
-    "table_of_contents": {
-        "color": "default"
-    }
-}
-
-{
-    "heading_1": {
-        "rich_text": [{"text": {"content": "Leg_assessment"} }]
-    }
-},
-{
-    "heading_2": {
-        "rich_text": [{"text": {"content": "leg_1"}}],
-        "is_toggleable": True
-    }
-}
-
-# data作成
-data.append(tableOfBlock)
+data = [
+    
+]
 
 num = 1
 leg_num = str(num)
 
-add_data = {
-    "heading_3": {
-        "rich_text": [{"text": {"content": leg_num}}],
-        "is_toggleable": True
+made_data = [
+    # head contents
+    {
+        "table_of_contents": {
+            "color": "default"
+        }
+    },
+    {
+        "heading_1": {
+	        "rich_text": [{"text": {"content": "Race Data"} }]
+	    }   
+    },
+    {
+        "table": {
+            "table_width": 2,
+            "has_column_header": False,
+            "has_row_header": True,
+            "children": [
+                   {
+                        "table_row": {
+                            "cells": [
+                                [
+                                    {
+                                        "text": {
+                                            "content": "column1 content",
+                                        }
+                                    }
+                                ],
+                                [
+                                    {
+                                        "text": {
+                                            "content": "column2 content",
+                                        }
+                                    }
+                                ]
+                            ]
+                        }
+                    },
+                    {
+                        "table_row": {
+                            "cells": [
+                                [
+                                    {
+                                        "text": {
+                                            "content": "テレイン",
+                                        }
+                                    }
+                                ],
+                                [
+                                    {
+                                        "text": {
+                                            "content": "where",
+                                        }
+                                    }
+                                ]
+                            ]
+                        }
+                    },
+                    {
+                        "table_row": {
+                            "cells": [
+                                [
+                                    {
+                                        "text": {
+                                            "content": "距離",
+                                        }
+                                    }
+                                ],
+                                [
+                                    {
+                                        "text": {
+                                            "content": "6km",
+                                        }
+                                    }
+                                ]
+                            ]
+                        }
+                    },
+                    {
+                        "table_row": {
+                            "cells": [
+                                [
+                                    {
+                                        "text": {
+                                            "content": "アップ",
+                                        }
+                                    }
+                                ],
+                                [
+                                    {
+                                        "text": {
+                                            "content": "300m",
+                                        }
+                                    }
+                                ]
+                            ]
+                        }
+                    },
+                    {
+                        "table_row": {
+                            "cells": [
+                                [
+                                    {
+                                        "text": {
+                                            "content": "タイム",
+                                        }
+                                    }
+                                ],
+                                [
+                                    {
+                                        "text": {
+                                            "content": "いくらか",
+                                        }
+                                    }
+                                ]
+                            ]
+                        }
+                    },
+                    {
+                        "table_row": {
+                            "cells": [
+                                [
+                                    {
+                                        "text": {
+                                            "content": "巡航",
+                                        }
+                                    }
+                                ],
+                                [
+                                    {
+                                        "text": {
+                                            "content": "いくらか",
+                                        }
+                                    }
+                                ]
+                            ]
+                        }
+                    },
+                    {
+                        "table_row": {
+                            "cells": [
+                                [
+                                    {
+                                        "text": {
+                                            "content": "ミス率",
+                                        }
+                                    }
+                                ],
+                                [
+                                    {
+                                        "text": {
+                                            "content": "めっちゃ%",
+                                        }
+                                    }
+                                ]
+                            ]
+                        }
+                    },
+                    {
+                        "table_row": {
+                            "cells": [
+                                [
+                                    {
+                                        "text": {
+                                            "content": "ミスタイム",
+                                        }
+                                    }
+                                ],
+                                [
+                                    {
+                                        "text": {
+                                            "content": "+18915730",
+                                        }
+                                    }
+                                ]
+                            ]
+                        }
+                    }
+            ]
+        }
+    },
+    {
+	    "heading_2": {
+	        "rich_text": [{"text": {"content": "MAP"}}],
+	    }
+	},
+    {
+	    "heading_2": {
+	        "rich_text": [{"text": {"content": "LOG"}}],
+	    }
+	},
+	{
+	    "heading_1": {
+	        "rich_text": [{"text": {"content": "Leg Assessment"} }]
+	    }
+	},
+	{
+	    "heading_2": {
+	        "rich_text": [{"text": {"content": "○->1"}}],
+	        "is_toggleable": True
+	    }
+	},
+    {
+        "heading_2": {
+	        "rich_text": [{"text": {"content": "1->2"}}],
+	        "is_toggleable": True
+	    }
+    },
+    { 
+        "heading_2": {
+	        "rich_text": [{"text": {"content": "2->3"}}],
+	        "is_toggleable": True
+	    }
+    },
+    { 
+        "heading_2": {
+	        "rich_text": [{"text": {"content": "3->4"}}],
+	        "is_toggleable": True
+	    }
+    },
+    { 
+        "heading_2": {
+	        "rich_text": [{"text": {"content": "4->5"}}],
+	        "is_toggleable": True
+	    }
+    },
+    { 
+        "heading_2": {
+	        "rich_text": [{"text": {"content": "5->6"}}],
+	        "is_toggleable": True
+	    }
+    },
+    { 
+        "heading_2": {
+	        "rich_text": [{"text": {"content": "6->◎"}}],
+	        "is_toggleable": True
+	    }
     }
-}
-data.append(add_data)
-
-print(data)
+]
