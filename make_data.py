414f3ee8709eb797fb9b8d6dd99243747bd1e692
HTmake-code <fi1er01hkr10ty@gmail.com>
2024-04-10 00:37:45 +0900

    make_dataの行数の削減。

--- make_data.py
+++ make_data.py
@@ -7,240 +7,35 @@ leg_num = str(num)
 
 made_data = [
     # head contents
-    {
-        "table_of_contents": {
-            "color": "default"
-        }
-    },
-    {
-        "heading_1": {
-	        "rich_text": [{"text": {"content": "Race Data"} }]
-	    }   
-    },
+    {"table_of_contents": {"color": "default"}},
+    {"heading_1": {"rich_text": [{"text": {"content": "Race Data"}}]}},
+    # table
     {
         "table": {
             "table_width": 2,
             "has_column_header": False,
             "has_row_header": True,
             "children": [
-                   {
-                        "table_row": {
-                            "cells": [
-                                [
-                                    {
-                                        "text": {
-                                            "content": "column1 content",
-                                        }
-                                    }
-                                ],
-                                [
-                                    {
-                                        "text": {
-                                            "content": "column2 content",
-                                        }
-                                    }
-                                ]
-                            ]
-                        }
-                    },
-                    {
-                        "table_row": {
-                            "cells": [
-                                [
-                                    {
-                                        "text": {
-                                            "content": "テレイン",
-                                        }
-                                    }
-                                ],
-                                [
-                                    {
-                                        "text": {
-                                            "content": "where",
-                                        }
-                                    }
-                                ]
-                            ]
-                        }
-                    },
-                    {
-                        "table_row": {
-                            "cells": [
-                                [
-                                    {
-                                        "text": {
-                                            "content": "距離",
-                                        }
-                                    }
-                                ],
-                                [
-                                    {
-                                        "text": {
-                                            "content": "6km",
-                                        }
-                                    }
-                                ]
-                            ]
-                        }
-                    },
-                    {
-                        "table_row": {
-                            "cells": [
-                                [
-                                    {
-                                        "text": {
-                                            "content": "アップ",
-                                        }
-                                    }
-                                ],
-                                [
-                                    {
-                                        "text": {
-                                            "content": "300m",
-                                        }
-                                    }
-                                ]
-                            ]
-                        }
-                    },
-                    {
-                        "table_row": {
-                            "cells": [
-                                [
-                                    {
-                                        "text": {
-                                            "content": "タイム",
-                                        }
-                                    }
-                                ],
-                                [
-                                    {
-                                        "text": {
-                                            "content": "いくらか",
-                                        }
-                                    }
-                                ]
-                            ]
-                        }
-                    },
-                    {
-                        "table_row": {
-                            "cells": [
-                                [
-                                    {
-                                        "text": {
-                                            "content": "巡航",
-                                        }
-                                    }
-                                ],
-                                [
-                                    {
-                                        "text": {
-                                            "content": "いくらか",
-                                        }
-                                    }
-                                ]
-                            ]
-                        }
-                    },
-                    {
-                        "table_row": {
-                            "cells": [
-                                [
-                                    {
-                                        "text": {
-                                            "content": "ミス率",
-                                        }
-                                    }
-                                ],
-                                [
-                                    {
-                                        "text": {
-                                            "content": "めっちゃ%",
-                                        }
-                                    }
-                                ]
-                            ]
-                        }
-                    },
-                    {
-                        "table_row": {
-                            "cells": [
-                                [
-                                    {
-                                        "text": {
-                                            "content": "ミスタイム",
-                                        }
-                                    }
-                                ],
-                                [
-                                    {
-                                        "text": {
-                                            "content": "+18915730",
-                                        }
-                                    }
-                                ]
-                            ]
-                        }
-                    }
+                {"table_row": {"cells": [[{"text": {"content": "column1 content"}}], [{"text": {"content": "column2 content"}}]]}},
+                {"table_row": {"cells": [[{"text": {"content": "テレイン"}}], [{"text": {"content": "where"}}]]}},
+                {"table_row": {"cells": [[{"text": {"content": "距離"}}], [{"text": {"content": "6km"}}]]}},
+                {"table_row": {"cells": [[{"text": {"content": "アップ"}}], [{"text": {"content": "300m"}}]]}},
+                {"table_row": {"cells": [[{"text": {"content": "タイム"}}], [{"text": {"content": "いくらか"}}]]}},
+                {"table_row": {"cells": [[{"text": {"content": "巡航"}}], [{"text": {"content": "いくらか"}}]]}},
+                {"table_row": {"cells": [[{"text": {"content": "ミス率"}}], [{"text": {"content": "めっちゃ%"}}]]}},
+                {"table_row": {"cells": [[{"text": {"content": "ミスタイム"}}], [{"text": {"content": "+18915730"}}]]}}
             ]
         }
     },
-    {
-	    "heading_2": {
-	        "rich_text": [{"text": {"content": "MAP"}}],
-	    }
-	},
-    {
-	    "heading_2": {
-	        "rich_text": [{"text": {"content": "LOG"}}],
-	    }
-	},
-	{
-	    "heading_1": {
-	        "rich_text": [{"text": {"content": "Leg Assessment"} }]
-	    }
-	},
-	{
-	    "heading_2": {
-	        "rich_text": [{"text": {"content": "○->1"}}],
-	        "is_toggleable": True
-	    }
-	},
-    {
-        "heading_2": {
-	        "rich_text": [{"text": {"content": "1->2"}}],
-	        "is_toggleable": True
-	    }
-    },
-    { 
-        "heading_2": {
-	        "rich_text": [{"text": {"content": "2->3"}}],
-	        "is_toggleable": True
-	    }
-    },
-    { 
-        "heading_2": {
-	        "rich_text": [{"text": {"content": "3->4"}}],
-	        "is_toggleable": True
-	    }
-    },
-    { 
-        "heading_2": {
-	        "rich_text": [{"text": {"content": "4->5"}}],
-	        "is_toggleable": True
-	    }
-    },
-    { 
-        "heading_2": {
-	        "rich_text": [{"text": {"content": "5->6"}}],
-	        "is_toggleable": True
-	    }
-    },
-    { 
-        "heading_2": {
-	        "rich_text": [{"text": {"content": "6->◎"}}],
-	        "is_toggleable": True
-	    }
-    }
+    {"heading_2": {"rich_text": [{"text": {"content": "MAP"}}]}},
+    {"heading_2": {"rich_text": [{"text": {"content": "LOG"}}]}},
+	{"heading_1": {"rich_text": [{"text": {"content": "Leg Assessment"}}]}},
+    # toggle
+	{"heading_2": {"rich_text": [{"text": {"content": "○->1"}}], "is_toggleable": True}},
+    {"heading_2": {"rich_text": [{"text": {"content": "1->2"}}], "is_toggleable": True}},
+    {"heading_2": {"rich_text": [{"text": {"content": "2->3"}}], "is_toggleable": True}},
+    {"heading_2": {"rich_text": [{"text": {"content": "3->4"}}], "is_toggleable": True}},
+    {"heading_2": {"rich_text": [{"text": {"content": "4->5"}}], "is_toggleable": True}},
+    {"heading_2": {"rich_text": [{"text": {"content": "5->6"}}], "is_toggleable": True}},
+    {"heading_2": {"rich_text": [{"text": {"content": "6->◎"}}], "is_toggleable": True}}
 ]
