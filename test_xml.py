39710c39574a2f8b24344b10bb0135b358bf8417
HTmake-code <fi1er01hkr10ty@gmail.com>
2024-05-04 21:47:22 +0900

    テキスト抽出。必要な部分の大枠。

--- test_xml.py
+++ test_xml.py
@@ -2,20 +2,27 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
+# 取得対象ページのURL
 url = "https://mulka2.com/lapcenter/lapcombat2/split-list.jsp?event=8292&file=1&class=0"
-tag = "高橋光"
 
-req = requests.get(url)
-req.encoding = req.apparent_encoding
-soup = BeautifulSoup(req.text, "html.parser")
+try:
+    req = requests.get(url)
+    req.encoding = req.apparent_encoding
+    soup = BeautifulSoup(req.text, "html.parser")
+except:
+    print("Error: get URL")
+print(req.status_code)
 
-result = re.findall('', soup, re.S)
-
-"""
-with open('output.txt', 'w', encoding='utf-8') as file:
+try:
     for script_tag in enumerate(soup.findAll('script')):
         script_tag_string = str(script_tag)
-        file.write(str(script_tag))
-"""
 
-print(req.status_code)
+    # 'runnerData['runnerName'] = '高橋 光';' と 'runnerList.push(runnerData);' の間のテキストを抽出する正規表現パターン
+    pattern = r"runnerData\['runnerName'\] = '高橋 光';(.*?)runnerList\.push\(runnerData\);"
+
+    matches = re.findall(pattern, script_tag_string, re.DOTALL)
+
+    for match in matches:
+        print(match)
+except:
+    print("Error: check data")
